980c44f364808dbfcbda5ab9c8194c36a6344def
ldaucorp <larry@springrainit.com>
2021-01-15 12:35:26 +0600

    added vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
-require('./bootstrap');
+import Vue from "vue";
+
+//Main pages
+import appComponent from "./vue/app.vue";
+
+const app = new Vue({
+    el: "#app",
+    components: { "app-component": appComponent }
+});
